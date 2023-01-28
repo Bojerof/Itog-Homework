5963e92e1c5ee1a6938b70d3e8fef726031206bc
Evgeniy Bobrov <bojeroff@gmail.com>
2023-01-28 11:37:54 +0200

    Version 1.2

--- Homework.cs
+++ Homework.cs
@@ -8,12 +8,12 @@
     }
     return array;
 }
-int FindSize(string[] array)
+int FindSize(string[] array, int findSize)
 {
     int size = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i].Length <= 3) size++;
+        if (array[i].Length <= findSize) size++;
     }
     return size;
 }
@@ -39,13 +39,22 @@ void ShowArray(string[] array)
 }
 int Number()
 {
-    System.Console.WriteLine("Enter size: ");
-    string bollSize = Console.ReadLine();
+    System.Console.WriteLine("Enter positiv number: ");
+    string? bollSize = Console.ReadLine();
     int size;
     if (int.TryParse(bollSize, out size) && size > 0)
         {
-    return size;
+            return size;
         }
+    else if (bollSize == "")
+    {
+        Console.WriteLine("Вы хотите выйти из программы?");
+        Console.WriteLine("Если да нажмите Y, если нет N");
+        char simb = Convert.ToChar(Console.ReadLine());
+        if (simb == 'Y' || simb == 'y') Environment.Exit(0);
+       
+        return Number();
+    }
     else
     {
         Console.Clear();
@@ -57,6 +66,8 @@ int Number()
 
 int size = Number();
 string[] array = CreatArray(size);
-int newSize = FindSize(array);
+Console.WriteLine("Элементы до скольки символов вы хотите увидеть?");
+int findSize = Number();
+int newSize = FindSize(array, findSize);
 string[] newArray = NewArray(array, newSize);
 ShowArray(newArray);
